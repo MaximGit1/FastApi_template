f06b18b8cdd0e91789426658215468890e8598e3
MaximGit1 <allq137x@gmail.com>
2024-11-06 22:18:24 +0300

    created user schemas

---
+++ Auth_service/src/adapters/schemas/users.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+
+from src.domain.models import UserDomain
+from src.domain.models.roles import RolePermission
+
+
+class Frog(BaseModel):
+    id: int | None = None
+    nickname: str = Field(max_length=15)
+    password: bytes | str | None = None
+    role: RolePermission | None = None
+    is_super_user: bool = False
+    is_active: bool = True
+
+    def to_model(self) -> UserDomain:
+        return UserDomain(
+            id=self.id if self.id else None,
+            nickname=self.nickname,
+            password=self.password,
+            role=self.role,
+            is_super_user=True if self.is_super_user else False,
+            is_active=True if self.is_active else False,
+        )
