3ec6d4e4683c9f857e914dea5f78a68c6e527f5f
MaximGit1 <allq137x@gmail.com>
2024-11-17 22:42:45 +0300

    created permissions service

---
+++ Auth_service/src/domain/services/permissions.py
@@ -0,0 +1,12 @@
+from src.domain.models import Permissions
+from src.domain.protocols import UserReaderProtocol
+
+class PermissionService:
+    def __init__(self, user_reader: UserReaderProtocol):
+        self._reader = user_reader
+
+    async def has_permission(self, user_id: int, permission: Permissions) -> bool:
+        user = await self._reader.get_user_by_id(user_id)
+        if not user:
+            return False
+        return user.role.has_permission(permission)
