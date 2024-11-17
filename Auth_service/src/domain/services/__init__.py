35cb5d841f7ca050548a327f44b64025dcab665e
MaximGit1 <allq137x@gmail.com>
2024-11-17 23:18:13 +0300

    created imports in services init file

---
+++ Auth_service/src/domain/services/__init__.py
@@ -0,0 +1,12 @@
+from permissions import PermissionService
+from jwt_token import AuthService
+from salt import SaltService
+from user import UserService
+
+
+__all__ = (
+    "PermissionService",
+    "AuthService",
+    "SaltService",
+    "UserService",
+)
