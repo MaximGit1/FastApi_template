0330943a4f8808c49c8623b499df391d8a124b77
MaximGit1 <allq137x@gmail.com>
2024-11-17 22:02:56 +0300

    created salt service

---
+++ Auth_service/src/domain/services/salt.py
@@ -0,0 +1,17 @@
+from src.domain.protocols import SaltProtocol
+
+
+class SaltService:
+    def __init__(
+        self, salt_repository: SaltProtocol,
+    ) -> None:
+        self._salt_repository = salt_repository
+
+    def hash_password(self, password: str) -> bytes:
+        return self._salt_repository.hash_password(password=password)
+
+    def validate_password(self, password: str, hashed_password: bytes) -> bool:
+        return self._salt_repository.validate_password(
+            password=password,
+            hashed_password=hashed_password
+        )
